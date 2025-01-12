ad5ad65db5c7781cb501c4187a88d6167d409409
Farheen Malak <115576023+FarheenMalak@users.noreply.github.com>
2025-01-12 11:17:44 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="card">
+      <h1>Counter App</h1>
+      <button>
+        Count is {count}
+      </button>
+      <div>
+        <button 
+          className="increase"
+          onClick={() => {
+            if (count >= 20) {
+              alert('Cannot exceed this value');
+            } else {
+              setCount(count + 1);
+            }
+          }}>
+          Increase
+        </button>
+        <button 
+          className="decrease"
+          onClick={() => setCount(count - 1)}>
+          Decrease
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
+
